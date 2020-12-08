670f81ad76d7bffd47677874f195dcb260aa5e30
Alexandre Leroux <alex@sherpa.one>
2020-12-08 18:29:11 +0100

    gestion des erreurs 405 et header allow

--- api/routes/tags_routes.js
+++ api/routes/tags_routes.js
@@ -185,4 +185,23 @@ router.post("/",async(req,res,next)=>{
 
 });
 
+
+const allowed_http_verbs = [];
+
+router.stack.forEach(route=>{
+    try {
+        const verb = route.route.stack[0].method;
+
+        if (!allowed_http_verbs.includes(verb)) allowed_http_verbs.push(verb);
+
+    } catch (error) {
+        console.error(error);
+    }
+})
+
+router.use(function(req, res, next) {
+    res.append('Allow', allowed_http_verbs.toString());
+    return res.status(405).json({message:"Method Not Allowed"});
+});
+
 module.exports = router;
--- api/routes/users_routes.js
+++ api/routes/users_routes.js
@@ -244,5 +244,22 @@ router.get('/:id/tags', isAuthorized, async (req, res, next) => {
 });
 
 
+const allowed_http_verbs = [];
+
+router.stack.forEach(route=>{
+    try {
+        const verb = route.route.stack[0].method;
+
+        if (!allowed_http_verbs.includes(verb)) allowed_http_verbs.push(verb);
+
+    } catch (error) {
+        console.error(error);
+    }
+})
+
+router.use(function(req, res, next) {
+    res.append('Allow', allowed_http_verbs.toString());
+    return res.status(405).json({message:"Method Not Allowed"});
+});
 
 module.exports = router;
